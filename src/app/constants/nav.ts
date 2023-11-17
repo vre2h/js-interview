e0bca5de485d57468ad465f33264e2a6b6bc8e2c
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2023-11-18 01:40:35 +0400

    initial commit

---
+++ src/app/constants/nav.ts
@@ -0,0 +1,14 @@
+export const navItems = [
+  {
+    title: "Resources",
+    href: "/resources",
+  },
+  {
+    title: "Questionnaires",
+    href: "/questionnaires",
+  },
+  {
+    title: "Companies",
+    href: "/companies",
+  },
+];
